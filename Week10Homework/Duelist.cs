8e31856c2d7e7597663c7bcfc29e696fc583a0fa
Chester Brock Jr <W7259378@student.miracosta.edu>
2021-12-13 01:29:53 -0800

    Add project files.

---
+++ Week10Homework/Duelist.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10Homework
+{
+    class Duelist
+    {
+        private String name;
+        private double probabilityOfHitting;
+        private bool isAlive = true;
+
+        public string Name { get => name; set => name = value; }
+        public double ProbabilityOfHitting { get => probabilityOfHitting; set => probabilityOfHitting = value; }
+        public bool IsAlive { get => isAlive; set => isAlive = value; }
+
+        // constructor 
+        public Duelist(String tempName, double tempProbability)
+        {
+            name = tempName;
+            probabilityOfHitting = tempProbability;
+        }
+        bool getAlive()
+        {
+
+            return isAlive;
+        }
+
+        private static Random rand = new Random(0);
+
+        public void shootAtTarget(Duelist target)
+        {
+            var rand = new Random();
+            if (this.probabilityOfHitting == 1)
+            {
+                target.IsAlive = false;
+                target.getAlive();
+            }
+
+            else if (rand.NextDouble() <= this.probabilityOfHitting)
+            {
+                target.IsAlive = false;
+                target.getAlive();
+            }
+            else
+            {
+                target.getAlive();
+            }
+
+        }
+    }
+}
---
+++ Week10Homework/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int winsA = 0;
+            int winsB = 0;
+            int winsC = 0;
+            int i = 0;
+
+            Duelist aaron = new Duelist("Aaron", (1.0 / 3.0));
+            Duelist bob = new Duelist("Bob", (1.0 / 2.0));
+            Duelist charlie = new Duelist("Charlie", 1.0);
+
+            while (i != 1000)
+            {
+                if (aaron.IsAlive == true)
+                {
+
+                    if (charlie.IsAlive == true)
+                    {
+                        aaron.shootAtTarget(charlie);
+                    }
+                    else if (bob.IsAlive == true)
+                    {
+                        aaron.shootAtTarget(bob);
+                    }
+                    else
+                    {
+                        winsA++;
+                    }
+
+                }
+                else if (bob.IsAlive == true)
+                {
+                    if (charlie.IsAlive == true)
+                    {
+                        bob.shootAtTarget(charlie);
+                    }
+                    else if (aaron.IsAlive == true)
+                    {
+                        bob.shootAtTarget(aaron);
+                    }
+                    else
+                    {
+                        winsB++;
+                    }
+                }
+                else
+                {
+                    if (bob.IsAlive == true)
+                    {
+                        charlie.shootAtTarget(bob);
+                    }
+                    else if (aaron.IsAlive == true)
+                    {
+                        charlie.shootAtTarget(aaron);
+                    }
+                    else
+                    {
+                        winsC++;
+                    }
+                }
+                Console.WriteLine(winsA);
+                Console.WriteLine(winsB);
+                Console.WriteLine(winsC);
+                i++;
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
